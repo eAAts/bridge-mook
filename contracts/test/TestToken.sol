848bc75236df5f34352af64025cf911f5ea03e47
mg12 <volibear1001@gmail.com>
2023-10-20 18:50:14 +0900

    feat: Add TestToken contract

---
+++ contracts/test/TestToken.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    address public bridge;
+
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {}
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, _amount);
+    }
+}
