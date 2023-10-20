88a15923eaaf8b5ff2b20a26b111e783ffcedf94
mg12 <volibear1001@gmail.com>
2023-10-20 23:35:08 +0900

    feat: Delete deployTestToken script

--- scripts/deployBridgeMookupMain.js
+++ scripts/deployBridgeMookupMain.js
@@ -1,21 +1,34 @@
 const {
     ethers
-} = require("hardhat");
-
-async function main() {
-    const [deployer] = await ethers.getSigners();
-
-    console.log("Deploying contracts with the account:", deployer.address);
-
+  } = require("hardhat");
+  
+  async function deployToken(name, symbol, decimals) {
+    const TestToken = await ethers.getContractFactory("TestToken");
+    const testToken = await TestToken.deploy(name, symbol, decimals);
+  
+    console.log(`${symbol} address: ${testToken.address}`);
+  }
+  
+  async function deployBridgeMookup() {
     const BridgeMookup = await ethers.getContractFactory("BridgeMookup");
     const bridgeMookup = await BridgeMookup.deploy();
-
+  
     console.log("BridgeMookup address:", bridgeMookup.address);
-}
-
-main()
+  }
+  
+  async function main() {
+    const [deployer] = await ethers.getSigners();
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    await deployToken("USD Circle", "USDC", 6);
+    await deployToken("Ethereum", "ETH", 18);
+  
+    await deployBridgeMookup();
+  }
+  
+  main()
     .then(() => process.exit(0))
     .catch((error) => {
-        console.error(error);
-        process.exit(1);
+      console.error(error);
+      process.exit(1);
     });
