10f42718c13e78eb2552c33491627406dabfe4a3
mg12 <volibear1001@gmail.com>
2023-10-20 19:18:52 +0900

    feat: Update BridgeMookup contract

--- contracts/BridgeMookup.sol
+++ contracts/BridgeMookup.sol
@@ -2,43 +2,90 @@
 pragma solidity ^0.8.9;
 
 import "./base/TokenManager.sol";
+import "./token/BridgeToken.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract BridgeMookup is TokenManager {
     event TokensSent(
+        address indexed targetToken,
+        address indexed recipient,
+        uint256 indexed targetChainId,
+        uint256 amount
+    );
+    event TokensReceived(
         address indexed token,
-        address indexed sender,
         address indexed recipient,
         uint256 amount
     );
 
-    event TokensReceived(
-        address indexed token,
-        address indexed sender,
-        address indexed recipient,
-        uint256 amount
-    );
+    struct TransferInfo {
+        address targetToken;
+        address recipient;
+        uint256 targetChainId;
+        uint256 amount;
+    }
+
+    mapping(uint256 => TransferInfo) public transferInfos;
+    uint256 public currentTransferId;
+    uint256 public processedId;
 
     function sendTokens(
         address tokenAddress,
         address recipient,
-        uint256 amount
+        uint256 amount,
+        uint256 targetChainId
     ) external payable {
-        if (tokenAddress == address(0)) {
-            require(msg.value == amount, "Sent ETH value does not match");
-            payable(recipient).transfer(amount);
+        if (!isGeneratedToken(tokenAddress)) {
+            if (tokenAddress == address(0)) {
+                require(msg.value == amount, "Sent ETH value does not match");
+            } else {
+                IERC20(tokenAddress).transferFrom(
+                    msg.sender,
+                    address(this),
+                    amount
+                );
+            }
         } else {
-            IERC20(tokenAddress).transferFrom(msg.sender, recipient, amount);
+            BridgeToken(tokenAddress).burn(msg.sender, amount);
         }
-        emit TokensSent(tokenAddress, msg.sender, recipient, amount);
+
+        TransferInfo memory newTransferInfo = TransferInfo({
+            targetToken: getTargetToken(tokenAddress),
+            recipient: recipient,
+            targetChainId: targetChainId,
+            amount: amount
+        });
+
+        ++currentTransferId;
+        transferInfos[currentTransferId] = newTransferInfo;
+
+        emit TokensSent(
+            getTargetToken(tokenAddress),
+            recipient,
+            targetChainId,
+            amount
+        );
     }
 
     function receiveTokens(
         address tokenAddress,
+        address recipient,
         uint256 amount
     ) external onlyOwner {
-        IERC20(tokenAddress).transfer(msg.sender, amount);
-        emit TokensReceived(tokenAddress, msg.sender, address(this), amount);
+        if (!isGeneratedToken(tokenAddress)) {
+            if (tokenAddress == address(0)) {
+                payable(recipient).transfer(amount);
+            } else {
+                IERC20(tokenAddress).transfer(recipient, amount);
+            }
+        } else {
+            BridgeToken(tokenAddress).mint(recipient, amount);
+        }
+        emit TokensReceived(tokenAddress, recipient, amount);
+    }
+
+    function processTransfer(uint256 _processedId) external onlyOwner {
+        processedId = _processedId;
     }
 }
